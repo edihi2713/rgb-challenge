7ab75d2a9c888e10606acb4ecd1fb3fc015716a1
Édison Arley Higuita Higuita <edihi2713@gmail.com>
2016-11-15 19:43:25 -0500

    deploy

--- app/controllers/competitors_controller.rb
+++ app/controllers/competitors_controller.rb
@@ -1,5 +1,24 @@
 class CompetitorsController < ApplicationController
   def index
-  	
+  	@competitors = Competitor.all.order(score: :desc).limit(10)
   end
+
+  def create
+  	email = params[:competitor][:email]
+  	score = params[:competitor][:score].to_i
+  	competitor = Competitor.find_by_email(email)
+
+  	if competitor
+  		competitor.update(competitor_params) if competitor.score < score
+  	else
+  		Competitor.create(competitor_params)
+  	end
+
+  	redirect_to competitors_path
+  end
+
+  private 
+  	def competitor_params
+  		params.require(:competitor).permit(:email, :score)
+  	end
 end
